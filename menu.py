a05e40fc78b9980cc4690df372af9f2f0f88c6ed
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-04-28 11:56:27 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
 import streamlit as st
 st.sidebar.write('Menu')
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
 st.title('Cambio')
 st.write('am')
