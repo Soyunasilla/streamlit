80f4517b22bc3f6d6e252cbbb3c5e5421089d20e
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-04-28 12:18:14 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,5 +15,9 @@ add_slider = st.sidebar.slider(
 )
 st.title('Cambio')
 st.write('am')
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
