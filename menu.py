c0d8fbc755b7feffd3e650dadefd588773e37b6a
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-04-28 11:57:52 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,5 +5,11 @@ add_selectbox = st.sidebar.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone')
 )
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
 st.title('Cambio')
 st.write('am')
