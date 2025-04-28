af30fce07038ea151c4483fd3ea80a544958264f
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-04-28 12:53:47 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ st.sidebar.write('Menu')
 # Add a slider to the sidebar:
 add_slider = st.sidebar.slider(
     'Select a range of values',
-    1, 56, (25.0, 75.0)
+    1, 56, (1, 56)
 )
 
 # Add a selectbox to the sidebar:
