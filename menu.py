1c4c4cc062274d23ca1676694d2cb6d4178e837d
Soyunasilla <105076969+Soyunasilla@users.noreply.github.com>
2025-04-28 12:52:22 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,16 +3,19 @@ import pandas as pd
 import numpy as np
 
 st.sidebar.write('Menu')
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
+
 # Add a slider to the sidebar:
 add_slider = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
 st.title('BACO')
 st.write('Propuesta de Dashboard Movil')
 
